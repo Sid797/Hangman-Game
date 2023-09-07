5c9267b5c7aaa2d162a30c4879d69ade43bbea34
Sid797 <siddhantchakraborty977@gmail.com>
2023-09-07 12:04:43 +0530

    hangman-game

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1>HANGMAN-GAME</h1>
+      <p>FIND THE WORD BY ENTERING LETTERS!</p>
+      <p>IF YOU GUESS WRONG 6 TIMES YOU LOSE!</p>
+
+    </>
+  )
+}
+
+export default Header
